8d2a54ecd1c9b6186f23f4949b1da3c8efb3a966
sean <sean.71@gmail.com>
2023-06-11 20:59:40 +0900

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
+import {BoardComponent} from "./board/board.component";
+import {MainComponent} from "./main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'board', component: BoardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BoardModule} from "./board/board.module";
+import {AuthModule} from "./auth/auth.module";
+
+
+// Routing setup
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BoardModule,
+    AuthModule,
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]

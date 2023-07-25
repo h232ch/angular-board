9aae13eb0b83e6b9c8833b2b429bd2f59a45978b
sean <sean.71@gmail.com>
2023-07-25 23:27:43 +0900

    Token -> Jwt

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Board} from "./models/Board";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpRequest} from "@angular/common/http";
 import {CookieService} from "ngx-cookie-service";
-import {Observable, tap, throwError} from "rxjs";
+import {mergeMap, Observable, tap, throwError} from "rxjs";
 import {Boards} from "./models/Boards";
 import {FormControl, ɵValue} from "@angular/forms";
 import {Rules} from "./models/Rules";
@@ -36,14 +36,6 @@ export class ApiService {
   //     'Authorization': `Token ${token}`,
   //   });
   // }
-
-  getAuthHeaders(): HttpHeaders {
-    const token = this.getToken();
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-  }
 
   login(username: ɵValue<FormControl<string | null>> |
     undefined, password: ɵValue<FormControl<string | null>> | undefined): Observable<any> {
@@ -80,182 +72,77 @@ export class ApiService {
     localStorage.removeItem('jwtRefreshToken');
   }
 
-  handleUnauthorizedError(): any {
-    const refreshToken = this.getRefreshToken();
-
-    if (refreshToken) {
-      this.refreshToken(refreshToken).pipe(
-        catchError((error) => {
-          console.error('Failed to refresh token:', error);
-          this.removeTokens();
-          // Redirect to login page or show an error message
-          return throwError('Authorization failed. Please log in again.');
-          this.router.navigate(['/']);
-        })
-      ).subscribe(
-        (response) => {
-          if (response.access) {
-            this.saveToken(response.access, refreshToken);
-            // Retry fetching the secret after token refresh
-          } else {
-            this.removeTokens();
-            // Redirect to login page or show an error message
-            throwError('Authorization failed. Please log in again.');
-            this.router.navigate(['/']);
-          }
-        }
-      );
-    } else {
-      // Redirect to login page or show an error message
-      return throwError('Authorization failed. Please log in again.');
-      this.router.navigate(['/']);
-    }
+  getAuthHeaders(): HttpHeaders {
+    const token = this.getToken();
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
   }
 
   getBoards(page = 1, search?: ɵValue<FormControl<null>> | undefined | string): Observable<any> {
     if (search) {
-      // return this.httpClient.get<Boards>(
-      //   `${this.boardListUrl}?page=${page}&search=${search}`, {
-      //     headers: this.getAuthHeaders()});
-
-      return this.httpClient.get<Boards>(
+      const request = this.httpClient.get<Boards>(
         `${this.boardListUrl}?page=${page}&search=${search}`, {
           headers: this.getAuthHeaders()
-        }).pipe(
-        catchError((error) => {
-          if (error.status === 401) {
-            return this.handleUnauthorizedError();
-          }
-          return throwError(error);
         })
-      );
+      return request
 
     } else {
-      return this.httpClient.get<Boards>(
+      const request = this.httpClient.get<Boards>(
         `${this.boardListUrl}?page=${page}`, {
           headers: this.getAuthHeaders()
-        }).pipe(
-        catchError((error) => {
-          if (error.status === 401) {
-            return this.handleUnauthorizedError();
-          }
-          return throwError(error);
         })
-      );
+      return request;
     }
   }
 
   getBoard(id: number): Observable<any> {
     const url = `${this.baseBoardUrl}${id}`
-    return this.httpClient.get<Board>(url, {
+    const request = this.httpClient.get<Board>(url, {
       headers: this.getAuthHeaders()
-    }).pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          return this.handleUnauthorizedError();
-        }
-        return throwError(error);
-      })
-    );
+    })
+    return request;
   }
 
   createdBoard(title: string, description: string, pub_date: Date): Observable<any> {
     const body = JSON.stringify({title: title, description: description, pub_date: pub_date});
     const request = this.httpClient.post(`${this.baseBoardUrl}`, body, {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.post(`${this.baseBoardUrl}`, body, {headers: this.getAuthHeaders()})
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
 
   updatedBoard(id: number, title: string, description: string): Observable<any> {
     const body = JSON.stringify({title: title, description: description});
     const request = this.httpClient.put(`${this.baseBoardUrl}${id}/`,
       body, {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.put(`${this.baseBoardUrl}${id}/`,
-            body, {headers: this.getAuthHeaders()})
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
 
   deletedBoard(id: number): Observable<any> {
     const request = this.httpClient.delete(`${this.baseBoardUrl}${id}/`,
       {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.delete(`${this.baseBoardUrl}${id}/`,
-            {headers: this.getAuthHeaders()});
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
 
   createdComment(id: number, comment: string, pub_date: Date): Observable<any> {
     const body = JSON.stringify({movie: id, comment: comment, pub_date: pub_date});
     const request = this.httpClient.post(`${this.baseUrl}api/comments/`, body,
       {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.post(`${this.baseUrl}api/comments/`, body,
-            {headers: this.getAuthHeaders()})
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
 
   updatedComment(id: number, comment: string): Observable<any> {
     const body = JSON.stringify({comment: comment});
     const request = this.httpClient.put(`${this.baseUrl}api/comments/${id}/`,
       body, {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.put(`${this.baseUrl}api/comments/${id}/`,
-            body, {headers: this.getAuthHeaders()});
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
 
   deletedComment(id: number): Observable<any> {
     const request = this.httpClient.delete(`${this.baseUrl}api/comments/${id}/`,
       {headers: this.getAuthHeaders()})
-    return request.pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-          this.httpClient.delete(`${this.baseUrl}api/comments/${id}/`,
-            {headers: this.getAuthHeaders()});
-        }
-        return throwError(error);
-      })
-    );
+    return request;
   }
-
-  // Token Authorization
-  // loginUser(authData: any) {
-  //   const body = JSON.stringify({username: authData.username, password: authData.password});
-  //   return this.httpClient.post(`${this.baseUrl}api/auth/`, body, {headers: this.getAuthHeaders()});
-  // }
 
   registerUser(authData: any): Observable<any> {
     const body = JSON.stringify({username: authData.username, password: authData.password});
@@ -277,42 +164,23 @@ export class ApiService {
     if (src || dst || port) {
       const request = this.httpClient.get<Rules>(`${this.baseUrl}api/rule?src=${src}&dst=${dst}&port=${port}`,
         {headers: this.getAuthHeaders()})
-      return request.pipe(
-        catchError((error) => {
-          if (error.status === 401) {
-            this.handleUnauthorizedError();
-            this.httpClient.get<Rules>(`${this.baseUrl}api/rule?src=${src}&dst=${dst}&port=${port}`,
-              {headers: this.getAuthHeaders()})
-          }
-          return throwError(error);
-        })
-      );
+      return request;
     } else {
       const request = this.httpClient.get<any>(`${this.baseUrl}api/rule/`,
         {headers: this.getAuthHeaders()})
-      return request.pipe(
-        catchError((error) => {
-          if (error.status === 401) {
-            this.handleUnauthorizedError();
-            this.httpClient.get<any>(`${this.baseUrl}api/rule/`,
-              {headers: this.getAuthHeaders()})
-          }
-          return throwError(error);
-        })
-      );
+      return request;
     }
   }
 
   getLoginUser() {
-    return this.httpClient.get<Boards>(
-      `${this.apiUrl}user/`, {
-        headers: this.getAuthHeaders()
-      }).pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.handleUnauthorizedError();
-        }
-        return throwError(error);
-      }))
+    const request = this.httpClient.get<Boards>(`${this.apiUrl}user/`,
+      { headers: this.getAuthHeaders()})
+    return request;
   }
+
+  // Token Authorization
+  // loginUser(authData: any) {
+  //   const body = JSON.stringify({username: authData.username, password: authData.password});
+  //   return this.httpClient.post(`${this.baseUrl}api/auth/`, body, {headers: this.getAuthHeaders()});
+  // }
 }
